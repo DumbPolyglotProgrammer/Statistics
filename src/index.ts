8b76e26d8cd6878721f28cffd8cc145e2bb6dde0
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-09-13 00:31:06 +0530

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { MatchReader } from "./MatchReader";
 import { Summary } from "./Summary";
 
-const matchReader = MatchReader.fromCsv("football.csv");
+const matchReader = MatchReader.fromCsv("assets/football.csv");
 const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 
 matchReader.load();
