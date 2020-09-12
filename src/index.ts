d00bdd30d8b688b7691ce77175c4e5cbf8fcbe2e
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-09-12 23:46:51 +0530

    test commit

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ import { CsvFileReader } from "./CsvFileReader";
 const reader = new CsvFileReader("assets/football.csv");
 const matches = reader.read();
 
-console.log(matches);
+console.log
+    (matches);
